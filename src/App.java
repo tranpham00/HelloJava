b4698c970cae81ccb4f7404378f9ad5cd6afddc0
tran pham <trannpham.05@gmail.com>
2023-08-24 10:45:18 -0500

    Changed the greeting

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Tran!");
+        System.out.println("Hello, Tran Pham!");
     }
 }
